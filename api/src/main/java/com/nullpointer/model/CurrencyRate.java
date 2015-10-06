9c098ab09e61b1eadfd28cf99376fe44c1ae10d9
night wish <woodythegreat@yandex.ru>
2015-10-07 01:02:58 +0300

    idea's files

---
+++ api/src/main/java/com/nullpointer/model/CurrencyRate.java
@@ -0,0 +1,14 @@
+package com.nullpointer.model;
+
+import java.util.Date;
+
+/**
+ * Created by night wish on 06.10.2015.
+ * Represents Currency Rate object
+ * Getters and Setters are not used because it is Data Transfer Object class
+ */
+public class CurrencyRate {
+    public String code;
+    public String rate;
+    public Date date;
+}
