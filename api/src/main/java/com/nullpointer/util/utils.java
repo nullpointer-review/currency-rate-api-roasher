4207e321c52970677bd610dff933de9da1243c91
Pavel Yurkin <pyurkin@cinimex.ru>
2015-10-08 17:55:08 +0300

    Add util class

---
+++ api/src/main/java/com/nullpointer/util/utils.java
@@ -0,0 +1,20 @@
+package com.nullpointer.util;
+
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by pyurkin on 10/8/2015.
+ */
+public class utils {
+
+    public static XMLGregorianCalendar getXMLGregorianCalendarNow() throws DatatypeConfigurationException {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        DatatypeFactory datatypeFactory = DatatypeFactory.newInstance();
+        XMLGregorianCalendar now = datatypeFactory.newXMLGregorianCalendar(gregorianCalendar);
+        return now;
+    }
+
+}
